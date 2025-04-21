5aa616915b23606fc839c605032846c4b96e0def
Himanshi Mahankal <himanshimahankal@gmail.com>
2025-04-21 15:37:41 +0530

    creat Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+        return <h1 className="todo-header">what you like To do </h1>
+}
+
+export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Header from "../components/Header";
+import TodoItem from "../components/TodoItem";
+import Button from "../components/Button";
+import "../style.css";
 
 export default function Home() {
-  return <h1 align="center">Welcome to React App</h1>;
-}
+  return (
+    <div className="todo-container">
+      <Header />
+      <TodoItem text="Eat"/> 
+      <TodoItem text="Code"/> 
+      <TodoItem text="Play"/> 
+      <TodoItem text="Stude"/> 
+      <TodoItem text="Sleep again"/> 
+      <Button />
+    </div>
+  );
+};
